e7e3cec32a1d04dd03c98dedc5e5073513c3cb1d
debianeiro <98065046+debianeiro@users.noreply.github.com>
2022-09-18 14:08:38 -0300

    Create 01-base-i3wm.sh

---
+++ 01-base-i3wm.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# i3wm essential
+
+sudo apt install xorg \
+                 lightdm \
+                 i3-wm \
+                 i3blocks \
+                 i3status \
+                 i3lock
+                 suckless-tools \
+                 j4-dmenu-desktop \
+                 nitrogen \
+                 rofi \
+                 arandr \
+                 caja \
+                 gvfs-backends \
+                 gvfs-fuse \
+                 xarchiver \
+                 p7zip-full \
+                 rar \
+                 unrar \
+                 xfce4-terminal \
+                 flameshot \
+                 xfce4-notifyd \
+                 xfce4-power-manager \
+                 #feh \
+                 wicd-gtk \
+                 wicd-curses \
+                 wicd-cli \
+                 gsimplecal \
+                 compton \
+                 numlockx \
+                 inxi \
+                 geany \
+                 vim \
+                 wmctrl \
+                 xdotool \
+                 xinput \
+                 lxinput \
+                 lxappearance \
+                 papirus-icon-theme \
+                 arc-theme \
+                 numix-gtk-theme \
+                 numix-icon-theme \
+                 numix-icon-theme-circle \
+                 breeze-cursor-theme \
+                 #irssi \
+                 ttf-mscorefonts-installer \
+                 fonts-open-sans \
+                 fonts-incosolata \
+                 neofetch \
+                 #synaptic \
+                 gdebi \
+                 htop \
+                 policykit-1-gnome \
+                 pulseaudio \
+                 pulseaudio-utils \
+                 pulseeffects \
+                 pavucontrol \
+                 #yad
+                 network-manager \
+                 system-config-printer \
+                 firmware-linux-nonfree \
+                 blueman \
+                 mousepad \
+                 cups \
+                 printer-driver-escpr \
+
