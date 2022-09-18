93467267daf8c508565079631d3d6fbb60efa7aa
debianeiro <98065046+debianeiro@users.noreply.github.com>
2022-09-18 14:34:57 -0300

    Create 02-apps-utils.sh

---
+++ 02-apps-utils.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Aplicativos e utilitários
+
+sudo apt install thunderbird \
+                 thunderbird-l10n-pt-br \
+                 firefox \
+                 firefox-l10n-ptbr \
+                 libreoffice \
+                 libreoffice-l10n-pt-br \
+                 libreoffice-gnome \
+                 libreoffice-style-breeze \
+                 #filezilla \
+                 #smplayer \
+                 #smplayer-l10n \
+                 #smplayer-themes \
+                 vlc \
+                 vlc-l10n \
+                 vlc-plugin-visualization
+                 chromiun \
+                 transmission \
+                 atril \
+                 poppler-utils \
+                 goldendict \
+                 font-manager \
+                 kazam \
+                 libreoffice-style-breeze \
+                 obs-studio \
+                 obs-plugins \
+                 keepassxc \
+                 pastebinit \
+                 gimp \
+                 gnome-calculator \
+                 youtube-dl \
+                 exfalso \
+                 quodlibet \
+                 telegram-desktop \
+                 sxiv \
+                 moc \
+                 gparted \
+                 
